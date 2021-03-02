087d052331d234f448c9386e02bc03d13095c2b1
Mateus Varela <mvarela038@gmail.com>
2021-03-01 21:19:49 -0300

    Aplicando lógica.

--- index.js
+++ index.js
@@ -14,15 +14,21 @@ function updateRangeInput(val) {
 /**
  * Pega os valores dos input's checkbox.
  */
+const inputRange = document.getElementById('range')
 const checkBox0 = document.getElementById('checkBox0')
 const checkBox1 = document.getElementById('checkBox1')
 const checkBox2 = document.getElementById('checkBox2')
 
 document.getElementById('btn-enviar').onclick = function () {
-  if (checkBox0.checked && checkBox1.checked && checkBox2.checked) {
-    console.log('aqui')
-  } else {
-    console.log(Math.floor(Math.random() * 10000))
+  if (checkBox0.checked && !checkBox1.checked && !checkBox2.checked) {
+    console.log('aqui1')
+  } else if(!checkBox0.checked && checkBox1.checked && !checkBox2.checked) {
+     console.log(Math.random().toString(36).substr(-inputRange.value))
+  }else if (!checkBox0.checked && !checkBox1.checked && checkBox2.checked){
+    console.log('aqui3');
+  }else if (checkBox0.checked && checkBox1.checked && checkBox2.checked) {
+    console.log('aqui4');
   }
+  console.log(inputRange.value);
 }
-console.log(checkBox0.checked, 'aqui')
+
